b60f11481cde7dd8d54aeb3a881548382c56b146
adamKachmar <adam.kachmar@robolyon.com>
2025-02-06 21:21:53 +0100

    add initial structure

---
+++ src/main/include/Constants.h
@@ -0,0 +1,7 @@
+#pragma once
+
+// Write ALL your robot-specific constants here
+// Use namespacing to group constants together depending on the subsystems
+// Example: namespace DriveConstants {}
+// Example for variable : constexpr int kMOTOR_DRIVE = 0;
+// Example for macro : #define SQUARE(x) x*x
